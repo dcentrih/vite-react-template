5df22baa3ac02413c7ea049303df9359f99edeae
Denis Centrih <centrih.denis@outlook.com>
2024-09-28 13:03:02 +0200

    Added base path config

--- src/lib/router.tsx
+++ src/lib/router.tsx
@@ -12,4 +12,6 @@ const routes: RouteObject[] = [
   },
 ];
 
-export const router = createBrowserRouter(routes);
+export const router = createBrowserRouter(routes, {
+  basename: import.meta.env.BASE_URL,
+});
